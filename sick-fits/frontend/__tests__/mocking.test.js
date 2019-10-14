27a7b834448ef58ac637011db67f1475c78a312e
Katie Wright <katie.wrght@gmail.com>
2019-10-14 17:23:00 +0200

    create demo mocks

---
+++ sick-fits/frontend/__tests__/mocking.test.js
@@ -0,0 +1,36 @@
+import { resolve } from 'dns';
+
+function Person(name, foods) {
+  this.name = name;
+  this.foods = foods;
+}
+
+Person.prototype.fetchFavFoods = function() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(this.foods), 2000);
+  });
+};
+
+describe('mocking learning', () => {
+  it('mocks a reg function', () => {
+    const fetchDogs = jest.fn();
+    fetchDogs('snickers');
+    expect(fetchDogs).toHaveBeenCalled();
+    expect(fetchDogs).toHaveBeenLastCalledWith('snickers');
+    fetchDogs('hugo');
+    expect(fetchDogs).toHaveBeenCalledTimes(2);
+  });
+
+  it('can create a person', () => {
+    const me = new Person('Katie', ['chips', 'chocolate']);
+    expect(me.name).toBe('Katie');
+  });
+
+  it('can fetch foods', async () => {
+    const me = new Person('Katie', ['chips', 'chocolate']);
+    // Mock the favFoods function
+    me.fetchFavFoods = jest.fn().mockResolvedValue(['sushi', 'ramen']);
+    const favFoods = await me.fetchFavFoods();
+    expect(favFoods).toContain('sushi');
+  });
+});
