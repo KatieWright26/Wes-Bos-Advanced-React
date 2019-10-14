ad0ca94726403f6c711eef1aa6806c5ea17c2799
Katie Wright <katie.wrght@gmail.com>
2019-10-14 17:55:47 +0200

    add snapshot tests to cartcount and items

--- sick-fits/frontend/__tests__/item.test.js
+++ sick-fits/frontend/__tests__/item.test.js
@@ -1,4 +1,6 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+
 import ItemComponent from '../components/Item';
 
 const fakeItem = {
@@ -11,24 +13,28 @@ const fakeItem = {
 };
 
 describe('<Item/>', () => {
-  it('renders and displays the price properly', () => {
+  it('renders and matches the snapshot', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const PriceTag = wrapper.find('PriceTag');
-    expect(PriceTag.children().text()).toBe('$67.89');
-    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
-  it('renders the image properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const image = wrapper.find('img');
-    expect(image.props().src).toBe(fakeItem.image);
-    expect(image.props().alt).toBe(fakeItem.title);
-  });
-  it('renders out the buttons properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const buttonList = wrapper.find('.buttonList');
-    expect(buttonList.children()).toHaveLength(3);
-    expect(buttonList.find('Link')).toHaveLength(1);
-    expect(buttonList.find('AddToCart')).toHaveLength(1);
-    expect(buttonList.find('DeleteItem')).toHaveLength(1);
-  });
+  //   it('renders and displays the price properly', () => {
+  //     const wrapper = shallow(<ItemComponent item={fakeItem} />);
+  //     const PriceTag = wrapper.find('PriceTag');
+  //     expect(PriceTag.children().text()).toBe('$67.89');
+  //     expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  //   });
+  //   it('renders the image properly', () => {
+  //     const wrapper = shallow(<ItemComponent item={fakeItem} />);
+  //     const image = wrapper.find('img');
+  //     expect(image.props().src).toBe(fakeItem.image);
+  //     expect(image.props().alt).toBe(fakeItem.title);
+  //   });
+  //   it('renders out the buttons properly', () => {
+  //     const wrapper = shallow(<ItemComponent item={fakeItem} />);
+  //     const buttonList = wrapper.find('.buttonList');
+  //     expect(buttonList.children()).toHaveLength(3);
+  //     expect(buttonList.find('Link')).toHaveLength(1);
+  //     expect(buttonList.find('AddToCart')).toHaveLength(1);
+  //     expect(buttonList.find('DeleteItem')).toHaveLength(1);
+  //   });
 });
